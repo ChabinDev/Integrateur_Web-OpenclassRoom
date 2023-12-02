138c134a67b5bc1a111c50496a044a5a523ee4ce
ChabinDev <142836013+ChabinDev@users.noreply.github.com>
2023-12-02 21:52:48 +0100

    Nettoyage Fichier script.js

--- Projet 5/assets/script.js
+++ Projet 5/assets/script.js
@@ -18,58 +18,6 @@ const slides = [
 	}
 ];
 
-const bannerImage = document.querySelector('.banner-img');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-const dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
-
-let current_index = 0;
-
-// Fonction pour mettre à jour les points indicateurs
-function updateDots(index) {
-    dots.forEach((dot, i) => {
-        if (i === index) {
-            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
-        } else {
-            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
-        }
-    });
-}
-
-//fonction pour mettre à jour les points indicateurs, l'image et le texte
-function updateCarousel() {
-	// correction du bug pour la première et la dernière image
-	if (current_index < 0) {
-		current_index = slides.length - 1;
-	} else if (current_index >= slides.length) {
-		current_index = 0;
-}
-
-// Mettre à jour l'image
-const imagePath = `./assets/images/slideshow/${slides[current_index].image}`;
-bannerImage.src = imagePath;
-bannerImage.alt = `Slide ${current_index + 1}`;
-
-// Mettre à jour le Text du Tagline
-const tagLine = slides[current_index].tagLine;
-document.querySelector('p').innerHTML = tagLine
-}
-
-//Gestonnaire d'évènement pour le clic sur la flèche Gauche
-arrowLeft.addEventListener("click", (e) => {
-	current_index = (current_index - 1);
-	updateCarousel(current_index, "left");
-	updateDots(current_index);
-})
-
-//Gestonnaire d'evènement pour le clic sur la flèche Droite
-arrowRight.addEventListener('click', (e) => {
-	current_index = (current_index + 1);
-	updateCarousel(current_index, "right");
-	updateDots(current_index);
-});
-
-
 
 updateCarousel(current_index, 'Démarrage');
 
