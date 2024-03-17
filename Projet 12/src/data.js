8a7c4ea37c642724be406d2651a881e309449aff
ChabinDev <chabin_972@hotmail.fr>
2024-03-17 15:29:51 +0100

    Maj

---
+++ Projet 12/src/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        title: 'React 1',
+        subtitle: 'Mern stack',
+        description:
+            'lorem20',
+        image: './components/png'
+        link: 'http://reactbootcamp.com'
+    },
+];
