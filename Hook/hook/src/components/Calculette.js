646d43e3386f985f1ecc177822d26b4522c7b9e3
ChabinDev <chabin_972@hotmail.fr>
2024-03-21 20:36:59 +0100

    useReducer

--- Hook/hook/src/components/Calculette.js
+++ Hook/hook/src/components/Calculette.js
@@ -9,27 +9,47 @@ const calculReducer = (state, action) => {
         case 'MULTIPLY':
             return {a:action, b:action, total: action.a * action.b, operation: 'MULT'};
         case 'DIVIDE':
-            return {a:action, b:action, total: action.a / action.b, operation: 'DIV'};
+            return { a: action, b: action, total: action.a / action.b, operation: 'DIV' };
+        case 'RESET':
+            return { a: 0, b: 0, operation: 'RESET'};
+        default:
+            return state;
     }
 
 }
 const Calculette = () => {
     const [a, setA] = useState(0);
-    const [b, setA] = useState(0);
+    const [b, setB] = useState(0);
     const [state, dispatch] = useReducer(calculReducer, { a, b });
 
-    const changeA = () => {
+    const changeA = (e) => {
         setA(Number(e.target.value));
     };
 
-    const changeB = () => {
+    const changeB = (e) => {
         setB(Number(e.target.value));
+    ;}
+    
+    const reset = (e) => {
+        setA(0);
+        setB(0);
+        dispatch({ type: 'RESET' });
     }
 
   return (
       <div>
-          <input type='text' value={a} onChange={changeA} />
-          <input type='text' value={b} onChange={changeB} />
+          <input type='text' value={a} onChange={changeA} /><br/>
+          <input type='text' value={b} onChange={changeB} /><br/>
+          <button onClick={() => dispatch({ type: 'ADD', a: a, b: b })}>+</button>
+          <button onClick={() => dispatch({ type: 'MULTIPLY', a: a, b: b })}>*</button>
+          <button onClick={() => dispatch({ type: 'DIVIDE', a: a, b: b })}>/</button>
+          <button onClick={reset}>Reset</button><br/>
+          {state.total}
+          <div>
+              <code>
+                  {JSON.stringify(state, null, 4)}
+              </code>
+          </div>
       </div>
   )
 }
