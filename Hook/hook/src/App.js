c96339ebd4d93bc519e7582d10fdf51139d02888
ChabinDev <chabin_972@hotmail.fr>
2024-03-21 19:15:55 +0100

    useEffect

--- Hook/hook/src/App.js
+++ Hook/hook/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import PostList from './components/PostList'
 //import Like from './components/Like'
-import FramworkList from './components/FramworkList'
+//import FramworkList from './components/FramworkList'
 
 const App = () => {
   return (
     <div className='App'>
       <header className='App-header'>
         {/* <Like /> */}
-      <FramworkList />
+        {/* <FramworkList /> */}
+        <PostList />
       </header>
     </div>
   )
--- Hook/hook/src/components/PostList.js
+++ Hook/hook/src/components/PostList.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const PostList = () => {
-  return (
-    <div>PostList</div>
+
+    const [posts, setPosts] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const [counterDisabled, setCounterdisabled] = useState(false);
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((res) => setPosts(res.data));
+    }, []);
+
+    useEffect(() => {
+        if (counter === 3) {
+            setCounterdisabled(true);
+        }
+    }, [counter]);
+
+    const resetCounter = () => {
+        setCounter(0);
+        setCounterdisabled(false);
+    }
+
+    return (
+        <>
+            <button onClick={(e) => setCounter(counter + 1)} disabled={counterDisabled}>{counter}</button>
+            <button onClick={resetCounter}>Reset</button>
+            
+            <hr/>
+        <h2>PostList</h2>
+        {
+                posts.length > 0 ? (posts.map(p => <div key={p.id}>{p.title}</div>)) : (
+            <h3>loading ....</h3>
+            )
+        } 
+      </>
   )
 }
 
