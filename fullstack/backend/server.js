b085ed53e8755173217e431c1275ecbb7127292e
ChabinDev <chabin_972@hotmail.fr>
2024-03-25 20:25:54 +0100

    MAj

--- fullstack/backend/server.js
+++ fullstack/backend/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const connectToMongo = require('./config/db');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const connectToMongo = require("./config/db");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = 5000;
 
 // Connexion à la BDD
@@ -8,11 +9,20 @@ connectToMongo();
 
 const app = express();
 
+// Authorisation CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
 // Middelware qui permet de traiter les données de la Request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/post', require('./routes/post.routes'));
+app.use("/post", require("./routes/post.routes"));
 
 //Lancer le serveur
-app.listen(port, () => console.log('Le Serveur a démarré sur le Port ' + port))
+app.listen(port, () => console.log("Le Serveur a démarré sur le Port " + port));
--- fullstack/frontend/src/App.js
+++ fullstack/frontend/src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import NewPost from "./components/NewPost";
+import Thread from "./components/Thread";
 
 const App = () => {
-  return <div></div>;
+  const [userId, setUserId] = useState("");
+
+  return (
+    <div className="app-container">
+      <div className="login">
+        <h3>Bonjour</h3>
+        <input
+          type="text"
+          placeholder="Pseudo"
+          onChange={(e) => setUserId(e.target.value)}
+        />
+      </div>
+      <NewPost userId={userId} />
+      <Thread userId={userId} />
+    </div>
+  );
 };
 
 export default App;
